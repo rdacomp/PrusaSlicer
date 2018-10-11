c9f7491be6e475a603cbe00005206142267f69a8
tamasmeszaros <meszaros.q@gmail.com>
2018-10-11 17:35:44 +0200

    Comments for later

--- xs/src/libslic3r/SLARotfinder.hpp
+++ xs/src/libslic3r/SLARotfinder.hpp
@@ -14,6 +14,12 @@ namespace sla {
   * The function should find the best rotation for SLA upside down printing.
   *
   * @param modelobj The model object representing the 3d mesh.
+  * @param accuracy The optimization accuracy from 0.0f to 1.0f. Currently,
+  * the nlopt genetic optimizer is used and the number of iterations is
+  * accuracy * 100000. This can change in the future.
+  * @param statuscb A status indicator callback called with the unsigned
+  * argument spanning from 0 to 100. May not reach 100 if the optimization finds
+  * an optimum before max iterations are reached.
   * @param stopcond A function that if returns true, the search process will be
   * terminated and the best solution found will be returned.
   *
