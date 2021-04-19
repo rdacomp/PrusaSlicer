ad86274da5277765cd3b70e90aaaf3761cd3db91
Filip Sykala <filip.sykala@prusa3d.cz>
2021-04-19 14:19:18 +0200

    Fix. Now correct check when 2 vector have same direction

--- src/libslic3r/SLA/SupportIslands/PointUtils.cpp
+++ src/libslic3r/SLA/SupportIslands/PointUtils.cpp
@@ -23,6 +23,18 @@ Slic3r::Point PointUtils::perp(const Point &vector)
 
 bool PointUtils::is_same_direction(const Point &dir1, const Point &dir2)
 {
-    return (is_majorit_x(dir1)) ? (dir1.x() > 0) == (dir2.x() > 0) :
-                                  (dir1.y() > 0) == (dir2.y() > 0);
+    // (is_majorit_x(dir1)) ? (dir1.x() > 0) == (dir2.x() > 0) :
+    //                        (dir1.y() > 0) == (dir2.y() > 0);
+    // Cant use majorit direction:
+    // [2] 750000 544907
+    // [2] 463525 -1426583
+    // !! bad idea
+    
+    // Cant use dot product for int value ==> dir1.dot(dir2)
+    // diferent int result for input
+    //[2] - 128707028 93448506 
+    //[2] 10475487 1662574
+    // may be overflow ??
+    
+    return dir1.cast<float>().dot(dir2.cast<float>()) > 0;
 }
